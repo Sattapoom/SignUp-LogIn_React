5ba0f9c2b78aca0dcbd09dd48edc51b8348de3b7
Sattapoom Tulyasuk <s6301012630192@kmutnb.ac.th>
2022-04-02 02:45:45 +0700

    Deploy on vercel

--- src/services/appService.js
+++ src/services/appService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "http://localhost:4001",
+    baseURL: "https://poom-snake-api.herokuapp.com",
     headers: {
         "Content-type": "application/json"
     }
